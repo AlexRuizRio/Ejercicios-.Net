e171e4e0009a38973d86ffdd4b5ab4df2d2ef74c
Alex Ruiz <140838623+AlexRuizRio@users.noreply.github.com>
2023-11-11 00:42:33 +0100

    Reloj finalizado

--- Reloj/Form1.Designer.cs
+++ Reloj/Form1.Designer.cs
@@ -36,7 +36,6 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.paisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.añadirPaisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.borrarPaisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBoxHora = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
@@ -44,83 +43,84 @@
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // contextMenuStrip2
             // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip2.Name = "contextMenuStrip2";
             this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
             // 
             // contextMenuStrip3
             // 
+            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip3.Name = "contextMenuStrip3";
             this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
             // 
             // contextMenuStrip4
             // 
+            this.contextMenuStrip4.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip4.Name = "contextMenuStrip4";
             this.contextMenuStrip4.Size = new System.Drawing.Size(61, 4);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.paisToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(428, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(571, 28);
             this.menuStrip1.TabIndex = 8;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // paisToolStripMenuItem
             // 
             this.paisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.añadirPaisToolStripMenuItem,
-            this.borrarPaisToolStripMenuItem});
+            this.añadirPaisToolStripMenuItem});
             this.paisToolStripMenuItem.Name = "paisToolStripMenuItem";
-            this.paisToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.paisToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
             this.paisToolStripMenuItem.Text = "Pais";
             this.paisToolStripMenuItem.Click += new System.EventHandler(this.paisToolStripMenuItem_Click_1);
             // 
             // añadirPaisToolStripMenuItem
             // 
             this.añadirPaisToolStripMenuItem.Name = "añadirPaisToolStripMenuItem";
-            this.añadirPaisToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.añadirPaisToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.añadirPaisToolStripMenuItem.Text = "Añadir Pais";
             this.añadirPaisToolStripMenuItem.Click += new System.EventHandler(this.añadirPaísToolStripMenuItem_Click);
             // 
-            // borrarPaisToolStripMenuItem
-            // 
-            this.borrarPaisToolStripMenuItem.Name = "borrarPaisToolStripMenuItem";
-            this.borrarPaisToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.borrarPaisToolStripMenuItem.Text = "Borrar Pais";
-            // 
             // textBoxHora
             // 
-            this.textBoxHora.Location = new System.Drawing.Point(85, 61);
+            this.textBoxHora.Location = new System.Drawing.Point(113, 75);
+            this.textBoxHora.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxHora.Name = "textBoxHora";
-            this.textBoxHora.Size = new System.Drawing.Size(239, 20);
+            this.textBoxHora.Size = new System.Drawing.Size(317, 22);
             this.textBoxHora.TabIndex = 9;
             this.textBoxHora.TextChanged += new System.EventHandler(this.textBoxHora_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 45);
+            this.label1.Location = new System.Drawing.Point(109, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(40, 16);
             this.label1.TabIndex = 10;
             this.label1.Text = "Hora:";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Reloj
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 344);
+            this.ClientSize = new System.Drawing.Size(571, 423);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxHora);
             this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Reloj";
             this.Text = "Reloj";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -140,7 +140,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem paisToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem añadirPaisToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem borrarPaisToolStripMenuItem;
         private System.Windows.Forms.TextBox textBoxHora;
         private System.Windows.Forms.Label label1;
     }
