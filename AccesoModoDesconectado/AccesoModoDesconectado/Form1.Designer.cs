62f349cbc22176a839c1fd5ee46a42bf3b64c3c6
Alex Ruiz <140838623+AlexRuizRio@users.noreply.github.com>
2024-05-20 17:44:57 +0200

    Inicio de ejercicio con la conexion a modo desconectado

---
+++ AccesoModoDesconectado/AccesoModoDesconectado/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace AccesoDesconectado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxTitulos = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.textBoxActores = new System.Windows.Forms.TextBox();
+            this.textBoxDirectores = new System.Windows.Forms.TextBox();
+            this.comboBoxPais = new System.Windows.Forms.ComboBox();
+            this.comboBoxClasificacion = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxTitulos
+            // 
+            this.listBoxTitulos.FormattingEnabled = true;
+            this.listBoxTitulos.ItemHeight = 15;
+            this.listBoxTitulos.Location = new System.Drawing.Point(13, 107);
+            this.listBoxTitulos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBoxTitulos.Name = "listBoxTitulos";
+            this.listBoxTitulos.Size = new System.Drawing.Size(388, 304);
+            this.listBoxTitulos.TabIndex = 0;
+            this.listBoxTitulos.SelectedIndexChanged += new System.EventHandler(this.listBoxTitulos_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 41);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.Image = global::AccesoDesconectado.Properties.Resources.icons8_nuevo_48;
+            this.toolStripMenuItem1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(60, 37);
+            this.toolStripMenuItem1.Text = "Nuevo";
+            this.toolStripMenuItem1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripMenuItem1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem2.Image = global::AccesoDesconectado.Properties.Resources.icons8_disk_911;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(69, 37);
+            this.toolStripMenuItem2.Text = "Guardar";
+            this.toolStripMenuItem2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripMenuItem2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem3.Image = global::AccesoDesconectado.Properties.Resources.icons8_borrar_64;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(58, 37);
+            this.toolStripMenuItem3.Text = "Borrar";
+            this.toolStripMenuItem3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripMenuItem3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem4.Image = global::AccesoDesconectado.Properties.Resources.icons8_consult_64;
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(79, 37);
+            this.toolStripMenuItem4.Text = "Consultar";
+            this.toolStripMenuItem4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripMenuItem4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(456, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Actores";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(456, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Directores";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(456, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pais";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(456, 391);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Calsificacion";
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(614, 107);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(357, 21);
+            this.textBoxTitulo.TabIndex = 8;
+            // 
+            // textBoxActores
+            // 
+            this.textBoxActores.Location = new System.Drawing.Point(614, 169);
+            this.textBoxActores.Name = "textBoxActores";
+            this.textBoxActores.Size = new System.Drawing.Size(357, 21);
+            this.textBoxActores.TabIndex = 9;
+            // 
+            // textBoxDirectores
+            // 
+            this.textBoxDirectores.Location = new System.Drawing.Point(614, 240);
+            this.textBoxDirectores.Name = "textBoxDirectores";
+            this.textBoxDirectores.Size = new System.Drawing.Size(357, 21);
+            this.textBoxDirectores.TabIndex = 10;
+            // 
+            // comboBoxPais
+            // 
+            this.comboBoxPais.FormattingEnabled = true;
+            this.comboBoxPais.Location = new System.Drawing.Point(614, 313);
+            this.comboBoxPais.Name = "comboBoxPais";
+            this.comboBoxPais.Size = new System.Drawing.Size(357, 23);
+            this.comboBoxPais.TabIndex = 11;
+            // 
+            // comboBoxClasificacion
+            // 
+            this.comboBoxClasificacion.FormattingEnabled = true;
+            this.comboBoxClasificacion.Location = new System.Drawing.Point(614, 391);
+            this.comboBoxClasificacion.Name = "comboBoxClasificacion";
+            this.comboBoxClasificacion.Size = new System.Drawing.Size(357, 23);
+            this.comboBoxClasificacion.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.Controls.Add(this.comboBoxClasificacion);
+            this.Controls.Add(this.comboBoxPais);
+            this.Controls.Add(this.textBoxDirectores);
+            this.Controls.Add(this.textBoxActores);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.listBoxTitulos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTitulos;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.TextBox textBoxActores;
+        private System.Windows.Forms.TextBox textBoxDirectores;
+        private System.Windows.Forms.ComboBox comboBoxPais;
+        private System.Windows.Forms.ComboBox comboBoxClasificacion;
+    }
+}
+
---
+++ AccesoModoDesconectado/AccesoModoDesconectado/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccesoDesconectado
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection conexion;
+        private OleDbCommandBuilder comandBuilder;
+        private OleDbDataAdapter adapterEmision, adapterPaises, adapterRatings;
+        private DataSet dataSet = new DataSet();
+        private DataTable tablaEmision, tablaPaises, tablaRatings;
+        private DataRowView registro;
+        private string _STRING_CONEXION = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\\temp\\GestionProgramacion.mdb";
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                cargarDataSet();
+
+                //rellamos los ComboBox
+                comboBoxClasificacion.DataSource = tablaRatings;
+                comboBoxClasificacion.DisplayMember = "Rating";
+
+                comboBoxPais.DataSource = tablaPaises;
+                comboBoxPais.DisplayMember = "DescPais";
+
+                //rellenamos el listBox
+                listBoxTitulos.Sorted = true;
+                listBoxTitulos.DataSource = tablaEmision;
+                listBoxTitulos.DisplayMember = "Titulo";
+
+            }
+
+        }
+        /*
+         * METODO PARA CONECTAR CON LA BASE DE DATOS Y CARGAR EL DATA SER,
+         * FINALMENTE CERRAMOS LA CONEXION
+         */
+        private void cargarDataSet()
+        {
+            //Abrimos la conexion
+            string query;
+            dataSet.Clear();
+            conexion = new OleDbConnection(_STRING_CONEXION);
+            conexion.Open();
+
+            //llenamos el dataSet con los distintos adapters y creamos las tablas
+            query = "SELECT * FROM Emisiones";
+            adapterEmision = new OleDbDataAdapter(query, conexion);
+            comandBuilder = new OleDbCommandBuilder(adapterEmision);
+            adapterEmision.Fill(dataSet, "Emisiones");
+            tablaEmision = dataSet.Tables["Emisiones"];
+
+            query = "SELECT * FROM Paises";
+            adapterPaises = new OleDbDataAdapter(query, conexion);
+            adapterPaises.Fill(dataSet, "Paises");
+            tablaPaises = dataSet.Tables["Paises"];
+
+            query = "SELECT * FROM Ratings";
+            adapterRatings = new OleDbDataAdapter(query, conexion);
+            adapterRatings.Fill(dataSet, "Ratings");
+            tablaRatings = dataSet.Tables["Ratings"];
+
+            //cerramos la conexion
+
+            conexion.Close();
+
+        }
+        private void listBoxTitulos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            registro = (DataRowView)listBoxTitulos.SelectedItem;
+            if (registro != null)
+            {
+                textBoxTitulo.Text = registro["Titulo"].ToString();
+                textBoxActores.Text = registro["Actores"].ToString();
+                textBoxDirectores.Text = registro["Director"].ToString();
+                comboBoxClasificacion.SelectedIndex = Convert.ToInt32(registro["IdRating"].ToString()) - 1;
+                /*
+                 * este try es porque en la base de datos hay valores en IdPais que no exiten en la tabla Paises.
+                 * Si vemos un valor que no se encuentra en la tabla Paises, marcamos como "NO DEFINIDO" el comboBox
+                 */
+                try
+                {
+                    comboBoxPais.SelectedIndex = Convert.ToInt32(registro["IdPais"].ToString()) - 1;
+                }
+                catch
+                {
+                    comboBoxPais.Text = "PAIS NO DEFINIDO";
+                }
+            }
+
+        }
+        //METODO QUE ACTUALIZA LA BASE DE DATOS Y VUELVE A CARGAR EL DATASET ACTUALIZADO
+        private void actualizarDb()
+        {
+            conexion.Open();
+            adapterEmision.Update(dataSet, "Emisiones");
+            dataSet.AcceptChanges();
+            conexion.Close();
+            cargarDataSet();
+        }
+
+        //METODO BOTON NUEVO
+
+    }
+}
